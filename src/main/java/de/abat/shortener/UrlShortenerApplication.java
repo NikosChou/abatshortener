87f7bfc7195f30725992418515870d3befeca5e2
choustni <nikolaos@choustoulakis.de>
2024-10-22 12:23:25 +0300

    revert back to spring-web

--- src/main/java/de/abat/shortener/UrlShortenerApplication.java
+++ src/main/java/de/abat/shortener/UrlShortenerApplication.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Slf4j
 @SpringBootApplication(exclude = RedisAutoConfiguration.class)
@@ -13,4 +15,13 @@ public class UrlShortenerApplication {
         SpringApplication.run(UrlShortenerApplication.class, args);
     }
 
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
 }
